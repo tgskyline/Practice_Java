9efb6fbb22ea627ce4986b8893c9faae6aeb418e
tgskyline <tigomes.santos@gmail.com>
2021-11-07 15:25:27 -0300

    If-Java

--- IfJava.java
+++ IfJava.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class IfJava {
   public static void main(String[] args) {
     
-  Scanner leitura =new Scanner(System.in);
+  Scanner leitura = new Scanner(System.in);
 
   int num;
   int par = 0;
