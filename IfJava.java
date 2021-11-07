81e1cd097abfa0c2fc592141795e80ef34fa029b
tgskyline <tigomes.santos@gmail.com>
2021-11-07 10:36:56 -0300

    If_Java

---
+++ IfJava.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class IfJava {
+  public static void main(String[] args) {
+    
+  Scanner leitura =new Scanner(System.in);
+
+  int num;
+  int par = 0;
+
+  do {
+    num = leitura.nextInt();
+    if ((num % 2) == 0) 
+    // O sinal % traz o resto da divisão
+    {
+      System.out.println("Número par detectado");
+      par++;  
+    }
+    else
+    {
+      System.out.println("Número ímpar dectado");
+    }
+  }while (num != -1);
+  // O sinal de diferente no Java é !=
+
+  System.out.println("Você digitou " + par + " número(s) par(es).");  
+  }
+}
--- JavaLoops_Tabuada.java
+++ JavaLoops_Tabuada.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class JavaLoops_Tabuada {
   public static void main(String[] args) {
             
