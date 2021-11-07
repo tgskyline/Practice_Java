bd985e4c111daed4750f4de429b4c8512d2cc50a
tgskyline <tigomes.santos@gmail.com>
2021-11-07 10:13:14 -0300

    JavaLoops_Tabuada

---
+++ JavaLoops_Tabuada.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class JavaLoops_Tabuada {
+  public static void main(String[] args) {
+            
+    for (int i = 1; i < 11; i++)
+    {
+      System.out.println("Tabuada do " + i);
+    
+      for (int j = 1; j < 11; j++)    
+    
+          System.out.println( i + " x " + j + " = " + (i *j));
+    }    
+  }
+}
