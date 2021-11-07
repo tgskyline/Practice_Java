b0c05da3cf4f87ef3b96b8e69025f24a6a997370
tgskyline <tigomes.santos@gmail.com>
2021-11-07 09:33:52 -0300

    Parameters

---
+++ Initial Java.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.ArrayList;
+
+class Main {
+
+    final static double PI = 3.14;
+
+    // Constantes são declarada em maiúsculo
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+    // int num1;
+    // num1 = 10;
+
+    // float num2;
+    // num2 = 20;
+
+    // float op = num1 + num2;
+
+  double raio = 2;
+  double areaCirculo = PI * (raio * raio); 
+
+    // Scanner leitura = new Scanner(System.in);
+    // System.out.print("Digite um número: ");
+
+    // // ln após o print pula uma linha
+
+    // int num = leitura.nextInt();
+    // num = num * 2;
+    
+    System.out.println("Resultado:"+ areaCirculo);
+    
+    // System.out.println("O primeiro número é:"+ op);
+  }
+}
